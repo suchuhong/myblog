29d53d485c788f10b0f1783ded9eb9ea48ec72d6
suchuhong <1442615457@qq.com>
2020-06-23 11:44:46 +0800

    更改 myblog.nginx

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ supervisorctl update
 #supervisorctl
 ln -s ~/var/www/myblog/config/myblog.nginx /etc/nginx/sites-available/myblog.nginx
 ln -s /etc/nginx/sites-available/myblog.nginx /etc/nginx/sites-enabled/myblog.nginx
+systemctl restart nginx
